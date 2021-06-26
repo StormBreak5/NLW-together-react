0b648cc69a57c0e8307cdfe27aaf502c3b5f16cf
Itzac Angelo Albertin <itzacalbertin@gmail.com>
2021-06-25 21:05:21 -0300

    fourth day

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,12 +13,12 @@ import { useState } from 'react';
 import { database } from '../services/firebase';
 
 export function Home() {
-    const history = useHistory();  
+    const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
     const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom() {
-        if(!user) {
+        if (!user) {
             await signInWithGoogle()
         }
 
@@ -28,21 +28,26 @@ export function Home() {
     async function handleJoinRoom(event: FormEvent) {
         event.preventDefault();
 
-        if(roomCode.trim() === '') {
+        if (roomCode.trim() === '') {
             return;
         }
 
         const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
-        if(!roomRef.exists()) {
+        if (!roomRef.exists()) {
             alert('Room does not exists.');
+            return;
+        }
+
+        if (roomRef.val().endedAt) {
+            alert('Room already closed')
             return;
         }
 
         history.push(`/rooms/${roomCode}`);
     }
 
-    return(
+    return (
         <div id='page-auth'>
             <aside>
                 <img src={illustrationImg} alt="Ilustração perguntas e respostas" />
@@ -58,12 +63,12 @@ export function Home() {
                     </button>
                     <div className='separator'>ou entre em uma sala</div>
                     <form>
-                        <input 
-                            type="text" 
+                        <input
+                            type="text"
                             placeholder="Digite o código da sala"
                             onChange={event => setRoomCode(event.target.value)}
                             value={roomCode}
-                            />
+                        />
                         <Button type="submit">
                             Entrar na sala
                         </Button>
